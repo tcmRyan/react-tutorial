6411eef3b11abb6bd18070701c6367509a5e495d
ryan-s <ryan@cloudlock.com>
2018-02-08 07:23:58 -0500

    debugging module complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
 // import Radium, { StyleRoot } from 'radium';
-
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
     state = {
@@ -60,13 +60,13 @@ class App extends Component {
           persons = (
               <div>
                   {this.state.persons.map((person, index) => {
-                      return <Person
+                      return <ErrorBoundary key={ person.id }><Person
                           click={() => this.deletePersonHandler(index)}
                           name={ person.name }
                           age={ person.age }
-                          key={ person.id }
+
                           changed={(event) => this.nameChangedHandler(event, person.id)}
-                      />
+                      /></ErrorBoundary>
                   })}
               </div>
           );
